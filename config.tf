d396f41e39d55ef87a86381924aba7bd97566e9a
msparmar4 <126829847+msparmar4@users.noreply.github.com>
2023-03-02 18:03:51 -0500

    Delete config.tf

--- config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "msparmar4"            // Bucket where to SAVE Terraform State
-    key    = "01-networking/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                       // Region where bucket is created
-  }
-}
